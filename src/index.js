8a7cd2fb3bc6b7dfe30c64c6e2af84d37772fdf6
Jayant Paliwal <montypaliwal.biz@gmail.com>
2017-05-09 21:43:53 +0530

    classwise
    ES6 Compitable

--- src/index.js
+++ src/index.js
@@ -1,79 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
+import App from './App';
 import './index.css';
 
-
-class Clock extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            date: new Date()
-        };
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        )
-
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
-    };
-
-}
-
-class Toggle extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isToggleOn: true
-        };
-        //this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick = () => {
-        this.setState(prevState => ({
-            isToggleOn: !prevState.isToggleOn
-        }))
-    };
-
-
-    render() {
-        return (
-            <button onClick={this.handleClick}>
-                {this.state.isToggleOn ? 'ON' : 'OFF'}
-            </button>
-        );
-    }
-}
-
-function App() {
-    return (
-        <div>
-            <Clock/>
-            <Toggle/>
-        </div>
-    );
-}
 
 
 ReactDOM.render(
