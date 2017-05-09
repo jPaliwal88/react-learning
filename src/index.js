180d6f2a0cee0e6edb126c7840db5215e2981008
Jayant Paliwal <montypaliwal.biz@gmail.com>
2017-05-09 21:00:56 +0530

    state and lifecycle

--- src/index.js
+++ src/index.js
@@ -3,66 +3,45 @@ import ReactDOM from 'react-dom';
 //import App from './App';
 import './index.css';
 
-function Comment(props) {
-    return (
-        <div className="Comment">
-            <UserInfo user={props.author} />
-            <div className="Comment-text">
-                {props.text}
+
+class Clock extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            date: new Date()
+        };
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        )
+
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
             </div>
-            <div className="Comment-date">
-                {formatDate(props.date)}
-            </div>
-        </div>
-    );
-}
-
-function Avatar(props) {
-    return (
-        <img className="Avatar"
-             src={props.user.avatarUrl}
-             alt={props.user.name}
-        />
-    );
-}
-
-
-
-function UserInfo(props) {
-    return (
-        <div className="UserInfo">
-            <Avatar user={props.user} />
-            <div className="UserInfo-name">
-                {props.user.name}
-            </div>
-        </div>
-    );
-}
-
-function formatDate(date) {
-    return date.toLocaleDateString();
-}
-
-
-function App() {
-    const comment = {
-        date: new Date(),
-        text: 'I hope you enjoy learning React!',
-        author: {
-            name: 'Hello Kitty',
-            avatarUrl: 'http://placekitten.com/g/64/64'
-        }
+        );
     };
 
-    return (
-        <Comment
-            date={comment.date}
-            text={comment.text}
-            author={comment.author} />
-    );
 }
 
 ReactDOM.render(
-    <App />,
+    <Clock /> ,
     document.getElementById('root')
 );
