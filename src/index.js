6a25d45aca794b6afaf9f1c4603e0d362b87d79f
Jayant Paliwal <montypaliwal.biz@gmail.com>
2017-05-09 21:31:27 +0530

    handling events

--- src/index.js
+++ src/index.js
@@ -41,7 +41,42 @@ class Clock extends React.Component{
 
 }
 
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isToggleOn: true
+        };
+        //this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick = () => {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }))
+    };
+
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
+    }
+}
+
+function App() {
+    return (
+        <div>
+            <Clock/>
+            <Toggle/>
+        </div>
+    );
+}
+
+
 ReactDOM.render(
-    <Clock /> ,
+    <App /> ,
     document.getElementById('root')
 );
